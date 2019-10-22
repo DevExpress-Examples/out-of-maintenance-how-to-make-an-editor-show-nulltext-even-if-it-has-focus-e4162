f847208cef70c8afcf95d62d55152cc3738a8253
ivansupport <yakovlev.ivan@corp.devexpress.com>
2019-10-22 17:10:59 +0300

    update

--- VB/NullTextInActiveEditor/Application.xaml.vb
+++ VB/NullTextInActiveEditor/Application.xaml.vb
@@ -1,16 +1,7 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Configuration
-Imports System.Data
-Imports System.Linq
-Imports System.Windows
+﻿Imports System.Windows
 
 Namespace NullTextInActiveEditor
-	''' <summary>
-	''' Interaction logic for App.xaml
-	''' </summary>
-	Partial Public Class App
-		Inherits Application
-	End Class
+    Partial Public Class App
+        Inherits Application
+    End Class
 End Namespace
